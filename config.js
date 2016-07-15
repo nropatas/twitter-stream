837f788796c4d039a463a7c8949b5c5b38fe64a9
Sam Sataponn <sam@abctech-thailand.com>
2016-07-15 15:54:55 +0700

    Implement module twitter for filtering tweets

--- config.js
+++ config.js
@@ -7,7 +7,11 @@ nconf.argv()
     .env()
     .defaults({
         NODE_ENV: nodeEnv,
-        PORT: 3000
+        PORT: 3000,
+        TWITTER_CONSUMER_KEY: 'A61XsF3kIZRa3g8SEWa4j8rA9',
+        TWITTER_CONSUMER_SECRET: '58dP59jpArcWzdGncG1N4VAdXooBJFKXxKhQ3mUPQu0VmmM7WR',
+        TWITTER_ACCESS_TOKEN_KEY: '137725279-s3MIPFqG5KC5wVlkSylBrHDhHd86olyNnxNJIn82',
+        TWITTER_ACCESS_TOKEN_SECRET: '3w2perh3qlEccmEwPxRKbAC7APm0x3mpG5UEPfjbQc3Uw'
     });
 
 module.exports = nconf;
