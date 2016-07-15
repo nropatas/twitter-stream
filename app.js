7cbd446a5488d10367b546eb6ec0877cb0357db1
Sam Sataponn <sam@abctech-thailand.com>
2016-07-15 11:34:52 +0700

    Add app configurations in app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const express = require('express');
+const app = express();
+
+const config = require('./config');
+const path = require('path');
+
+app.set('port', config.get('PORT'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+app.use('/static', express.static('public'));
+app.use('/', require('./routes'));
+
+app.listen(app.get('port'), () => {
+    console.log('Server is listening to port ' + app.get('port'));
+});
