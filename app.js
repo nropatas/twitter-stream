7aae103585466a47484e574829aeeb4fdc4b2b13
Sam Sataponn <sam@abctech-thailand.com>
2016-07-18 15:43:45 +0700

    Implement page index which shows filtered tweets
    - Things that are shown include avatars, text, and dates only.

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const app = express();
 
 const config = require('./config');
 const path = require('path');
+const hbs = require('hbs');
+const moment = require('moment');
+
+hbs.registerHelper('formatDate', (date) => {
+    return moment(date).format('MMMM Do YYYY, h:mm:ss a');
+});
 
 app.set('port', config.get('PORT'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
-app.engine('html', require('hbs').__express);
+app.engine('html', hbs.__express);
 app.use('/static', express.static('public'));
 app.use('/', require('./routes'));
 
--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,21 @@ const model = require('../models/index');
 
 function index() {}
 
-index.prototype.fetch = (req, res) => {
+index.prototype.showFeed = (req, res) => {
+    if (req.query.track) {
+        model.getTweetsFromDb(req.query.track)
+            .then((rows) => {
+                res.render('index', {
+                    data: rows
+                });
+            })
+            .catch(console.error);
+    } else {
+        throw new Error('track was not specified');
+    }
+};
+
+index.prototype.fetch = (req) => {
     if (req.query.track) {
         model.fetch(req.query.track);
     } else {
