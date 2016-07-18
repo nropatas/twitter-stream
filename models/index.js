f3f4c38a215976d2cb83f5f0a205acccc6aa08d9
Sam Sataponn <sam@abctech-thailand.com>
2016-07-18 11:20:15 +0700

    Implement a function for querying tweets containing the keyword

--- models/index.js
+++ models/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const config = require('../config');
 const db = require('./db');
 const twitter = require('./twitter');
 
 function index() {}
 
 index.prototype.getTweetsFromDb = (keyword) => {
-
+    return db(config.get('TABLE_NAME')).where(db.raw(`LOWER(text) LIKE ?`, `%${keyword}%`))
+        .orderBy('id', 'desc');
 };
 
 index.prototype.fetch = (keyword) => {
