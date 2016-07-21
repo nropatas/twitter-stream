5087fcb0b32d77a5a9c88a01e2790bbbee83d773
Sam Sataponn <sam@abctech-thailand.com>
2016-07-21 15:57:01 +0700

    Add tweet_id to the schema

--- migrations/20160715114709_createDevTables.js
+++ migrations/20160715114709_createDevTables.js
@@ -4,6 +4,7 @@ exports.up = function(knex) {
     return knex.schema
         .createTable('tweets', (table) => {
             table.increments();
+            table.string('tweet_id');
             table.string('username');
             table.string('name');
             table.string('avatar');
