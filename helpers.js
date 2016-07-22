9a5b49ab5e0816897ef6c612c1abcdd5ae36250e
Sam Sataponn <sam@abctech-thailand.com>
2016-07-22 15:10:05 +0700

    Remove an unused helper

--- helpers.js
+++ helpers.js
@@ -23,26 +23,6 @@ hbs.registerHelper('lastCol', (index, options) => {
     return options.inverse(this);
 });
 
-hbs.registerHelper("math", function(lvalue, operator, rvalue, options) {
-    if (arguments.length < 4) {
-        // Operator omitted, assuming "+"
-        options = rvalue;
-        rvalue = operator;
-        operator = "+";
-    }
-
-    lvalue = parseFloat(lvalue);
-    rvalue = parseFloat(rvalue);
-
-    return {
-        "+": lvalue + rvalue,
-        "-": lvalue - rvalue,
-        "*": lvalue * rvalue,
-        "/": lvalue / rvalue,
-        "%": lvalue % rvalue
-    }[operator];
-});
-
 hbs.registerHelper('formatDate', (date) => {
     let now = moment();
     let tweetDate = moment(date);
